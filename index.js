9650991e990ac32728504d3627c57625f85506be
seyilonge <seyilonge@gmail.com>
2017-12-28 15:58:06 -0500

    Test run...

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-export { default as Button } from './dist/1.0.0/button'
-export { default as Footer } from './dist/1.0.0/footer'
+//export { default as Button } from './dist/1.0.0/button'
+//export { default as Footer } from './dist/1.0.0/footer'
+import Button from './dist/1.0.0/button'
+export Button as Button
