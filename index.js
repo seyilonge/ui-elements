85f29751d05cd30ab9ad193e29381dac61e2bde1
seyilonge <seyilonge@gmail.com>
2018-01-02 14:36:59 -0500

    Test run...

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-//export { default as Button } from './dist/1.0.0/button'
+export { default as Button } from './dist/1.0.0/button'
 //export { default as Footer } from './dist/1.0.0/footer'
 //const Button = require('./dist/1.0.0/button')
 //module.exports = Button;
 //export Button as Button
-import Button from './dist/1.0.0/button'
-export { Button }
+//import Button from './dist/1.0.0/button'
+//export { Button }
